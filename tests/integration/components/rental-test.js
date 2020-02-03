f5f5fcb93d9495f13bddb49d0e2295650602a896
mpascualach <miguelpacheson@hotmail.es>
2020-02-04 00:00:54 +0100

    more about map components

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -6,21 +6,14 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  test('it renders information about a rental property', async function(assert) {
     await render(hbs`<Rental />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Rental>
-        template block text
-      </Rental>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('article').hasClass('rental')
+    assert.dom('article h3').hasText('Grand Old Mansion')
+    assert.dom('article .detail.owner').hasText('Owner: Veruca Salt')
+    assert.dom('article .detail.type').hasText('Type: Standalone')
+    assert.dom('article .detail.location').hasText('Location: San Francisco')
+    assert.dom('article .detail.bedrooms').hasText('Number of bedrooms: 15')
   });
 });
